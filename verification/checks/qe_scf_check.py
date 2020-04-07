c5b5e9984e611f8add471d668e96220c96cfa759
toxa81 <toxa81@gmail.com>
2020-04-07 16:45:25 +0200

    move NiO to verification

--- verification/checks/qe_scf_check.py
+++ verification/checks/qe_scf_check.py
@@ -259,7 +259,7 @@ class qe_Au_surf_scf(qe_scf_base_test):
         self.time_limit = '20m'
 
 @rfm.parameterized_test(*([variant, ranks] for variant in ['native', 'sirius'] for ranks in [(4,1), (8,1)]))
-class qe_Si63Ge_scf(qe_scf_base_test):
+class qe_HfNi5_scf(qe_scf_base_test):
     def __init__(self, variant, ranks):
         super().__init__(ranks[0], ranks[1], 'HfNi5', 'pw.in', variant,
             energy_ref=-1829.32458388,
@@ -268,6 +268,17 @@ class qe_Si63Ge_scf(qe_scf_base_test):
             forces_ref=[[-0.06025067, -0.01561183, -0.00866171], [-0.00930775, -0.01289289, -0.00783269],
                        [-0.00342628, -0.01078834, -0.07976882], [-0.03836225, 0.11493676, 0.06015426],
                        [0.01053608, -0.02871813, -0.00419844], [0.10081087, -0.04692557, 0.04030739]])
+        self.tags = {'qe-%s'%variant, 'parallel'}
+
+
+@rfm.parameterized_test(*([variant, ranks] for variant in ['native', 'sirius'] for ranks in [(3,1), (5,1)]))
+class qe_NiO_afm_scf(qe_scf_base_test):
+    def __init__(self, variant, ranks):
+        super().__init__(ranks[0], ranks[1], 'NiO-afm', 'nio_afm.scf.in', variant,
+            energy_ref=-769.7684178,
+            P_ref=-76.05,
+            stress_ref=[[-0.000517, -3.172e-05, -3.172e-05], [-3.172e-05, -0.000517, -3.172e-05], [-3.172e-05, -3.172e-05, -0.000517]],
+            forces_ref=[[-0.0, 0.0, -0.0], [0.0, 0.0, 0.0], [-2.4e-07, -2.4e-07, -2.4e-07], [2.4e-07, 2.4e-07, 2.4e-07]])
         self.tags = {'qe-%s'%variant, 'parallel'}
 
 
